eae42ba64fd0c838bc328d66cfb545d9ea5fa6de
kalexoudis <kons98@aol.com>
2021-04-12 19:07:33 +0200

    Initial commit

---
+++ src/actions/main.java
@@ -0,0 +1,18 @@
+package actions;
+
+import game.GameClock;
+import game.GameMaster;
+import gui.Gui;
+
+public class main {
+
+    public static void main(String[] args) {
+        Gui g = new Gui();
+        GameMaster gm = new GameMaster();
+        GameClock gc = new GameClock();
+
+        gm.setup();
+        g.create();
+        gc.start();
+    }
+}
